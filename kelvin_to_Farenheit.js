14114c84ece77297aa03badaca57310267fb9473
LesegoSE <lesego.seikali@gmail.com>
2024-02-28 20:11:50 +0200

    adding Kelvin to Farenheight code

---
+++ kelvin_to_Farenheit.js
@@ -0,0 +1,13 @@
+'Variables'
+var kelvinTemp = 301;
+var celciusTemp;
+var fahrenheit;
+
+'Converts the Kelvin temperature reading to Celcius reading'
+celciusTemp = kelvinTemp - 273.15;
+
+'Calculates fahrenheit temperature value'
+fahrenheit = celciusTemp * (9 / 5) + 32;
+
+'Rounds down the value of Fahrenhight'
+fahrenheit = Math.floor(fahrenheit);
