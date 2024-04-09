734be2cdc30df4b0eb707e06ae5e294bc613a2e2
LesegoSE <lesego.seikali@gmail.com>
2024-04-09 20:32:45 +0200

    Human years to Cat years

--- Human_to_Cat_Years.js
+++ Human_to_Cat_Years.js
@@ -9,8 +9,10 @@ var rem_hum_yrs;
 //Removing first 2 initial years from human years
 rem_hum_yrs = hum_Years - 2;
 
+
 rem_hum_yrs *= 4;
 
 cat_yrs = rem_hum_yrs + intial_2_cat_yrs;
+
 var sentence = 'My human age is ' + hum_Years + ' and I am ' + cat_yrs + ' years old in cat years.'
 console.log(sentence);
