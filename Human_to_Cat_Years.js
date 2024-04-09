9d98c6ae8341ab3d493438bb8d45b707a7a537ac
LesegoSE <lesego.seikali@gmail.com>
2024-04-09 20:37:13 +0200

    adding comments

--- Human_to_Cat_Years.js
+++ Human_to_Cat_Years.js
@@ -1,18 +1,24 @@
-//variables
+//Human age
 var hum_Years = 20;
+
+//Initial 2 cat years in human years
 var intial_2_cat_yrs = 50;
 
-var cat_yrs;
+//Stores the remaining human years after subtracting 2 initial Cat years from it
 var rem_hum_yrs;
 
+//Stores the calculated Cat years 
+var cat_yrs;
 
 //Removing first 2 initial years from human years
 rem_hum_yrs = hum_Years - 2;
 
-
+//multiplying the remaining human years by 4 to account for the cat years after the 2 initial years
 rem_hum_yrs *= 4;
 
+// adding the remaining human years with the initial 2 cat yeasr to give us the final cat years.
 cat_yrs = rem_hum_yrs + intial_2_cat_yrs;
 
+// Prints out a sentence
 var sentence = 'My human age is ' + hum_Years + ' and I am ' + cat_yrs + ' years old in cat years.'
 console.log(sentence);
