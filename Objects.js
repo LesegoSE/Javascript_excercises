1b779271f5fcce2e436b3c5c51430acbf19ba047
LesegoSE <lesego.seikali@gmail.com>
2024-05-14 21:34:22 +0200

    Adding Object.js file

---
+++ Objects.js
@@ -0,0 +1,21 @@
+// regular variable declaration
+var vehicle = 'VW golf';
+console.log(vehicle);
+
+// Creating new object literal
+var vehicleParts = {
+    part1 : 'Engine',
+    part2 : 'Wheels',
+    part3 : 'Doors',
+    part4 : 'Headlights'
+};
+
+//Displaying objet literal
+console.log(vehicleParts);
+
+// Adding a new key pair
+vehicleParts.part5 = 'Windows';
+
+//Displaying objet literal
+console.log('\nAfter Adding Part 5 \n');
+console.log(vehicleParts);
