1123a4f88e389574aab610d70f53a830e9e93ba5
LesegoSE <lesego.seikali@gmail.com>
2024-05-29 19:02:21 +0200

    practicing the use of array methods

--- Arrays.js
+++ Arrays.js
@@ -2,14 +2,23 @@
 
 // Creating an array
 var movies = [ 'Avengers Endgame', 'One Punch Man', 'Blue Beetle', 'Huanted House', 'Tsotsi', 'Fantastic Four' ];
-console.log(movies);
-
-// Adding new records to the last element of the array
-movies.push('Toy Story');
-console.log('\n After adding new movie to the array\n =======================')
 console.log('\n Array elements: ' + movies);
 
-// removing last element of the array
-var frstElmnt = movies.shift();
-console.log('\n After removing last element of the array\n =======================')
-console.log(' Dropped movie: ' + frstElmnt + '\n Array elements: ' + movies + '\n');
+// Adding new records to the end of the array
+var mov = 'Toy Story';
+var mov1 = 'Fast and The Furious';
+movies.push(mov, mov1);
+console.log('\n After adding new movie to the array\n =======================');
+console.log('Added movies: ' + mov + ', ' + mov1);
+console.log(' Array elements: ' + movies);
+
+// Removing last element of the array
+var lstElmnt = movies.shift();
+console.log('\n After removing last element of the array\n =======================');
+console.log(' Dropped movie: ' + lstElmnt + '\n Array elements: ' + movies + '\n');
+
+// Adding new element to the front of the array
+var frstElmnt = 'Robot Boi';
+movies.unshift(frstElmnt);
+console.log('\n After adding element to the begining of an array\n =======================');
+console.log(' Added movie: ' + frstElmnt + '\n Array elements: ' + movies + '\n');
