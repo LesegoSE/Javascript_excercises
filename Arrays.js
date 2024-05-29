d9583765451a1764426c24f18246195594040c79
LesegoSE <lesego.seikali@gmail.com>
2024-05-29 19:13:19 +0200

    addeing slice function code

--- Arrays.js
+++ Arrays.js
@@ -9,16 +9,25 @@ var mov = 'Toy Story';
 var mov1 = 'Fast and The Furious';
 movies.push(mov, mov1);
 console.log('\n After adding new movie to the array\n =======================');
-console.log('Added movies: ' + mov + ', ' + mov1);
+console.log(' Added movies: ' + mov + ', ' + mov1);
 console.log(' Array elements: ' + movies);
 
-// Removing last element of the array
-var lstElmnt = movies.shift();
-console.log('\n After removing last element of the array\n =======================');
-console.log(' Dropped movie: ' + lstElmnt + '\n Array elements: ' + movies + '\n');
+// Removing first element of the array
+var frstDropElmnt = movies.shift();
+console.log('\n After removing first element of the array\n =======================');
+console.log(' Dropped movie: ' + frstDropElmnt + '\n Array elements: ' + movies + '\n');
 
 // Adding new element to the front of the array
-var frstElmnt = 'Robot Boi';
-movies.unshift(frstElmnt);
+var frstAddElmnt = 'Robot Boi';
+movies.unshift(frstAddElmnt);
 console.log('\n After adding element to the begining of an array\n =======================');
-console.log(' Added movie: ' + frstElmnt + '\n Array elements: ' + movies + '\n');
+console.log(' Added movie: ' + frstAddElmnt + '\n Array elements: ' + movies + '\n');
+
+// Removing last element in a array
+var lstDropElmnt = movies.pop();
+console.log('\n After removing last element of the array\n =======================');
+console.log(' Dropped movie: ' + lstDropElmnt + '\n Array elements: ' + movies + '\n');
+
+// Extracting array the elements in the range 2 - 4 using Slice() function
+var slyArr = movies.slice(1, 5);
+console.log('\n Array elements: ' + slyArr);
