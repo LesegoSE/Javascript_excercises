be27bb7dccf4bfdbc21c26e9cdcffcf459353bbe
LesegoSE <lesego.seikali@gmail.com>
2024-06-08 21:24:26 +0200

    code cleaning up

--- Arrays.js
+++ Arrays.js
@@ -28,28 +28,37 @@ var lstDropElmnt = movies.pop();
 console.log(' After removing last element of the array\n =======================');
 console.log(' Dropped movie: ' + lstDropElmnt + '\n Array elements: ' + movies + '\n');
 
-// Extracting array elements in the range 2 - 4 using Slice() function
+// Extracting array elements using Slice() function
 var slyArr = movies.slice(1, 5);
+var revSlyArr = movies.slice(-4,-1);
 console.log(' Extracting element of the array in range 2 - 4\n =======================');
-console.log(' Extraxted Array elements: ' + slyArr + '\n');
+console.log(' Normal slice array elements: ' + slyArr);
+console.log(' Reverse slice array elements: ' +  revSlyArr);
 
 // Creating an array that stores integers
 var dailyAudiance = [301, 42, 108, 500, 20, 450, 158, 76];
-console.log('\nNumeric array of Cinema daily audiance ' );
-console.log('\n Array elements: ' + dailyAudiance );
+console.log('\n Creating numeric array\n =======================' );
+console.log(' Array elements: ' + dailyAudiance );
 
-var mergeArr = dailyAudiance.concat(movies);
-console.log('\n Concatenated Array elements: ' + mergeArr );
+// Creating an array
+console.log('\n Numeric array sorting\n =======================');
 
-console.log( '\nAscending numeric Array elements : '+ dailyAudiance.sort(function(a, b)
+// Numeric array sorting 
+console.log( ' Ascending numeric Array elements : '+ dailyAudiance.sort(function(a, b)
 {return a-b; }
 ));
-console.log( '\nDecending numeric Array elements : '+ dailyAudiance.sort(function(a, b)
+console.log( ' Decending numeric Array elements : '+ dailyAudiance.sort(function(a, b)
 {return b-a; }
 ));
 
-
-console.log('\nAscending string array elements: ' + movies.sort());
-console.log( '\nDescending string array elements: '+ movies.sort(function(a, b)
+// String array sorting
+console.log('\n String array sorting\n =======================');
+console.log('\n Ascending string array elements: ' + movies.sort());
+console.log( '\n Descending string array elements: '+ movies.sort(function(a, b)
 {return b.localeCompare(a); }
 ));
+
+// Merging 2 arrays
+console.log('\n Array concatenation\n =======================');
+var mergeArr = dailyAudiance.concat(movies);
+console.log('\n Concatenated Array elements: ' + mergeArr );
