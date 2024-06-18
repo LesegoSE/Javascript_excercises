fcf7f4fbc6f8b89c97459290411ff99f992f55f5
LesegoSE <lesego.seikali@gmail.com>
2024-06-18 20:36:04 +0200

    complement generator code

--- complement_generator.js
+++ complement_generator.js
@@ -2,7 +2,8 @@ var randomBdyPrt = ['hair', 'eyes', 'hands', 'teeth', 'nails'];
 var randomAdj = ['nice', 'beautiful', 'lovely', 'cute'];
 var randomWords = ['You have', 'I like your', 'I am in love with your'];
 
-var bodyPrt = randomBdyPrt[Math.floor(Math.random() * 4)];
+var bodyPrt = randomBdyPrt[Math.floor(Math.random() * 5)];
 var adj = randomAdj[Math.floor(Math.random() * 4)];
-var word = randomWords[Math.floor(Math.random() * 4)];
+var word = randomWords[Math.floor(Math.random() * 3)];
 
+console.log(word + " " + adj + " " + bodyPrt);
