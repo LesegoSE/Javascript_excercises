b71de25acb5846d12b98cb4b9d9d418b20c355e7
unknown <lesego.seikali@gmail.com>
2024-10-08 15:36:05 +0200

    calorie calculator program

--- calorie_calc/calorie_calculator.js
+++ calorie_calc/calorie_calculator.js
@@ -1,2 +1,17 @@
-var nme = prompt("what: ");
-console.log(nme);
+/*
+Author: Lesego Seikali
+Description: Calorie calculator
+Date: 2024-10-08
+*/
+
+// User input
+var idealDailyCalories = 2;
+var arrActualDailyCalories = [];
+
+function inputidealDailyCalories (totalIdealDailyCalories){ // accepts user ideal daily calories input
+
+    totalIdealDailyCalories = idealDailyCalories * 7; //calculates total ideal daily calories
+    return totalIdealDailyCalories;
+};
+
+console.log(inputidealDailyCalories(idealDailyCalories))
