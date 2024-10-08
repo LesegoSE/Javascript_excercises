710c1befefc2b638323745e81247959f1bc3089b
unknown <lesego.seikali@gmail.com>
2024-10-08 16:33:08 +0200

    calorie calculator program

--- calorie_calc/calorie_calculator.js
+++ calorie_calc/calorie_calculator.js
@@ -5,7 +5,7 @@ Date: 2024-10-08
 */
 
 // User input
-var idealDailyCalories = 2;
+var idealDailyCalories = 5;
 var arrActualDailyCalories = [];
 
 function inputidealDailyCalories (totalIdealDailyCalories){ // accepts user ideal daily calories input
@@ -14,4 +14,52 @@ function inputidealDailyCalories (totalIdealDailyCalories){ // accepts user idea
     return totalIdealDailyCalories;
 };
 
-console.log(inputidealDailyCalories(idealDailyCalories))
+// console.log(inputidealDailyCalories(idealDailyCalories))
+
+
+function inputActualDailyCaloriesByDay(day){ 
+    
+    if (day == "monday") {
+        arrActualDailyCalories[0] = 3;
+    }
+    else if (day == "tuesday") {
+        arrActualDailyCalories[1] = 4;
+    }
+    else if (day == "wednesday") {
+        arrActualDailyCalories[2] = 5;
+    }
+    else if (day == "thursday") {
+        arrActualDailyCalories[3] = 4;
+    }
+    else if (day == "friday") {
+        arrActualDailyCalories[4] = 7;
+    }
+    else if (day == "saturday") {
+        arrActualDailyCalories[5] = 2;
+    }
+    else if (day == "sunday") {
+        arrActualDailyCalories[6] = 6;
+    } else{ "incorrect day entered!!";} 
+};
+
+inputActualDailyCaloriesByDay('monday'); // populates the actual calorie array
+inputActualDailyCaloriesByDay('tuesday'); // populates the actual calorie array
+inputActualDailyCaloriesByDay('wednesday'); // populates the actual calorie array
+inputActualDailyCaloriesByDay('thursday'); // populates the actual calorie array
+inputActualDailyCaloriesByDay('friday'); // populates the actual calorie array
+inputActualDailyCaloriesByDay('saturday'); // populates the actual calorie array
+inputActualDailyCaloriesByDay('sunday'); // populates the actual calorie array
+
+// console.log(arrActualDailyCalories);
+
+function calcActualDailyCalories (){
+
+    var totalActualyCalories = 0;
+    for(let i = 0;i < arrActualDailyCalories.length; i++){
+
+        totalActualyCalories += arrActualDailyCalories[i];
+    }
+    return totalActualyCalories;
+};
+
+// console.log(calcActualDailyCalories());
